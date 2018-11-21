13c6f1a99f3ba218592d9d6e9b079e853036bb09
Brad Niedt <bradleyniedt@gmail.com>
2018-11-21 12:45:45 -0700

    Adds check guess method, guess variable, and message variable to evaluate the guesses.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+NUMBER = rand(100)
+
+def check_guess(string_num)
+  if string_num.to_i < NUMBER && (string_num.to_i - NUMBER).abs > 5
+    "Way too low!"
+  elsif string_num.to_i > NUMBER && (string_num.to_i - NUMBER).abs > 5
+    "Way too high!"
+  elsif string_num.to_i < NUMBER
+    "Too low!"
+  elsif string_num.to_i > NUMBER
+    "Too high!"
+  elsif string_num.to_i == NUMBER
+    "You got it right! The secret number is #{NUMBER}"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:number => NUMBER, :message => message}
 end
